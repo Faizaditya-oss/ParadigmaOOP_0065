5b3671e59a8fc2908826c0afa6d88ccefa7a9c1d
Faizaditya-oss <faizaditya750@gmail.com>
2026-04-20 17:40:12 +0700

    Tambahkan var nim, nama, nilai.

--- IntroductionOOP.cpp
+++ IntroductionOOP.cpp
@@ -8,6 +8,11 @@ public:
     float nilai;
 
     void printData(){
-        
+        cout << "Nim: " << nim << endl;
+        cout << "Nama: " << nama << endl;
+        cout << "Nilai: " << nilai << endl;
+
     }
+
 };
+
